d4fd7d51bcc59cea86cb68db0836c35d1f6a1fe7
34r7h <2566560+34r7h@users.noreply.github.com>
2021-04-21 21:52:20 +0400

    source of knowledge && secret code

--- Desktop/gitpush.sh
+++ Desktop/gitpush.sh
@@ -1,3 +1,4 @@
 $ git add .
-$ git commit -m "this is the trickiest but I don't fucking get it"
+$ git commit -m "this is the trickiest but I don't fucking get it" 
+# https://dev.to/shriaas2898/taking-command-line-arguments-using-flags-in-bash-121 <--- ffswchbfh
 $ git push -u origin main
